53e54e7115ec73a0c3fec2c49218ef8a26278e12
vishal690 <vishal690@chouhan@.com>
2022-11-22 15:29:58 +0530

    signin commit

--- app/src/main/java/eu/tutorials/myreadingzone/DashBoard.java
+++ app/src/main/java/eu/tutorials/myreadingzone/DashBoard.java
@@ -2,13 +2,36 @@ package eu.tutorials.myreadingzone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashBoard extends AppCompatActivity {
+    TextView uid, email;
+    ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        email = (TextView) findViewById(R.id.viewEmail);
+        uid = (TextView) findViewById(R.id.viewUid);
+
+        email.setText("Email :- "+getIntent().getStringExtra("email").toString());
+        uid.setText("UID :- "+getIntent().getStringExtra("Uid").toString());
+    }
+
+    public void logOutHere(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(DashBoard.this,MainActivity.class));
     }
 }
